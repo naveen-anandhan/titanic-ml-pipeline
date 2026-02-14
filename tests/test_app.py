41a0e98fc40b3d0d1fc5b1811d0415c66e74e5f0
Naveen Anandhan <160575974+naveen-anandhan@users.noreply.github.com>
2026-02-14 12:43:36 +0530

    fix test import

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,8 @@
 from fastapi.testclient import TestClient
-from main import app
+from app import app
 
 client = TestClient(app)
 
-
-#test 
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
